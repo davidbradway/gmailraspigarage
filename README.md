30912127b71b2d3bdde053f385981f967db6c1a8
David Bradway <david.bradway@gmail.com>
2014-09-17 22:21:38 -0400

    add mac address checking, try for five minutes, if MAC is seen, open door

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 - need a filter that labels incoming messages that meet some critera with the label 'garage'
 - when a new message gets labeled that way, if it came from the address listed in the python script, then activate the door
 
+## Installation
+
+To run the script are required Python and Nmap, which can be found at the following : 
+
+* https://www.python.org/downloads/
+
+* http://nmap.org/download.html
+
+Also, take a look here : https://pypi.python.org/pypi/python-nmap
+
+    wget https://pypi.python.org/packages/source/p/python-nmap/python-nmap-0.3.4.tar.gz
+    tar xvfz python-nmap-0.3.4.tar.gz 
+    rm python-nmap-0.3.4.tar.gz 
+    wget https://github.com/mpescimoro/WiFinder/archive/master.zip
+    unzip master.zip 
+    rm master.zip 
+    sudo apt-get install nmap
--- raspigmail.py
+++ raspigmail.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+"""
+
+"""
+import sys
+import os
+import nmap                         # import nmap.py module
+import time
+
+WATCHEDMAC = "F4:F1:E1:41:12:02"
 
 ISPI = 1
 DEBUG = 0
@@ -13,6 +24,15 @@ if ISPI:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GARAGE, GPIO.OUT)
 
+try:
+    nm = nmap.PortScanner()         # instantiate nmap.PortScanner object
+except nmap.PortScannerError:
+    print('Nmap not found', sys.exc_info()[0])
+    sys.exit(1)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    sys.exit(1)
+
 # my unread messages never goes to zero, yours might
 NEWMAIL_OFFSET = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom/garage")["feed"]["fullcount"])
 while True:
@@ -23,10 +43,28 @@ while True:
     if newmails > NEWMAIL_OFFSET: 
         # if the newest email was from me, then open garage
         if feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom/garage")["entries"][-1]["author_detail"]["email"]  == USERNAME + '@gmail.com':
-            print "garage move"
+            print "start looking for phone before garage move"
             if ISPI:
-                GPIO.output(GARAGE, False)
-                time.sleep(1)
-                GPIO.output(GARAGE, True)
+                for x in range(0, 60):
+                    print "Try %d" % (x)
+                    found = 0
+                    nm.scan(hosts='192.168.1.1-18', arguments='-sP -n')
+                    for h in nm.all_hosts():
+                         # Vendor list for MAC address
+                         if 'mac' in nm[h]['addresses']:
+                             #print(nm[h]['addresses']['mac'])
+                            if nm[h]['addresses']['mac'] == WATCHEDMAC:
+                                print "found MAC: " + nm[h]['addresses']['mac']
+                                # open door
+                                GPIO.output(GARAGE, False)
+                                time.sleep(1)
+                                GPIO.output(GARAGE, True)
+                                found = 1
+                                break
+                    if found == 1:
+                        break
+                    else:
+                        print "not found"
+                        time.sleep(5)
     NEWMAIL_OFFSET = newmails
     time.sleep(MAIL_CHECK_FREQ)
