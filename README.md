402e47b03e2a5b14feea4846c59b0d367f4bac0a
David Bradway <david.bradway@gmail.com>
2014-09-20 18:33:51 -0400

    add flag for debugging without moving door

--- README.md
+++ README.md
@@ -7,11 +7,10 @@
 
 To run the script are required Python and Nmap, which can be found at the following : 
 
-* https://www.python.org/downloads/
-
-* http://nmap.org/download.html
-
-Also, take a look here : https://pypi.python.org/pypi/python-nmap
+- https://www.python.org/downloads/
+- http://nmap.org/download.html
+- Also, take a look here : https://pypi.python.org/pypi/python-nmap
+- Commands:
 
     wget https://pypi.python.org/packages/source/p/python-nmap/python-nmap-0.3.4.tar.gz
     tar xvfz python-nmap-0.3.4.tar.gz 
@@ -24,6 +23,7 @@ Also, take a look here : https://pypi.python.org/pypi/python-nmap
     sudo cp raspigmail.sh /etc/init.d/
     sudo chmod 755 /etc/init.d/raspigmail.sh 
     sudo update-rc.d raspigmail.sh defaults
+
     sudo /etc/init.d/raspigmail.sh start
     sudo /etc/init.d/raspigmail.sh status
     sudo /etc/init.d/raspigmail.sh stop
--- raspigmail.py
+++ raspigmail.py
@@ -2,23 +2,24 @@
 # -*- coding: latin-1 -*-
 
 """
-
+Script to check gmail for a new message with label 'Garage', when phone connects to wifi afterwards, open garage door
 """
 import sys
 import os
-import nmap                         # import nmap.py module
+import nmap                # import nmap.py module dependency
 import time
+import feedparser
 
-WATCHEDMAC = "F4:F1:E1:41:12:02"
 
-ISPI = 1
-DEBUG = 0
-USERNAME = "" # just the part before the @ sign, add yours here
-PASSWORD = ""
-GARAGE = 0 #Raspberry Pi GPIO Pin used for relay
+ISPI            = 1        # Is this being run on a Raspberry Pi?
+DEBUG           = 0        # Should I print Debugging text output?
+USERNAME        = "" # just the part before the @ sign, add yours here
+PASSWORD        = ""
+WATCHEDMAC      = ""
+GARAGE          = 0        # Raspberry Pi GPIO Pin used for relay
 MAIL_CHECK_FREQ = 10       # check mail interval wait [sec]
+MOVE_YES        = 0        # Should I actually move the door?
 
-import feedparser, time
 if ISPI:
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
@@ -35,6 +36,7 @@ except:
 
 # my unread messages never goes to zero, yours might
 NEWMAIL_OFFSET = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom/garage")["feed"]["fullcount"])
+
 while True:
     newmails = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom/garage")["feed"]["fullcount"])
     if DEBUG:
@@ -43,25 +45,26 @@ while True:
     if newmails > NEWMAIL_OFFSET: 
         # if the newest email was from me, then open garage
         if feedparser.parse("https://" + USERNAME + ":" + PASSWORD +"@mail.google.com/gmail/feed/atom/garage")["entries"][-1]["author_detail"]["email"]  == USERNAME + '@gmail.com':
-            print "start looking for phone before garage move"
+            print "Wait 5 minutes for phone to connect"
             if ISPI:
                 for x in range(0, 60):
-                    print "Try %d" % (x)
+                    print "Try %d" % (x+1)
                     found = 0
                     nm.scan(hosts='192.168.1.1-18', arguments='-sP -n')
                     for h in nm.all_hosts():
-                         # Vendor list for MAC address
-                         if 'mac' in nm[h]['addresses']:
-                             #print(nm[h]['addresses']['mac'])
+                        if 'mac' in nm[h]['addresses']:
+                            #print(nm[h]['addresses']['mac'])
                             if nm[h]['addresses']['mac'] == WATCHEDMAC:
                                 print "found MAC: " + nm[h]['addresses']['mac']
                                 # open door
-                                GPIO.output(GARAGE, False)
-                                time.sleep(1)
-                                GPIO.output(GARAGE, True)
+                                if MOVE_YES:
+                                    print "moving door"
+                                    GPIO.output(GARAGE, False)
+                                    time.sleep(1)
+                                    GPIO.output(GARAGE, True)
                                 found = 1
                                 break
-                    if found == 1:
+                    if found:
                         break
                     else:
                         print "not found"
