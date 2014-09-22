c07141f715a512aa91838ba06fd7411d7f3ca228
David Bradway <david.bradway@gmail.com>
2014-09-22 18:58:24 -0400

    add a new case to the autorestart script to account for a different daemon service message

--- autorestart.sh
+++ autorestart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-service raspigmail.sh status| grep FAIL > /dev/null 2>&1
+service raspigmail.sh status| grep 'FAIL\|failed' > /dev/null 2>&1
 if [ $? != 0 ]
 then
     echo "Still running"
